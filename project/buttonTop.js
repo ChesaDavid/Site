a07e6ed93415d2728240402fb853f2c96ef229d3
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-04-02 12:23:54 +0300

    to-top-button in process

--- project/buttonTop.js
+++ project/buttonTop.js
@@ -1,2 +1,15 @@
-var button = document.getElementById("impButton")
-     
+let button = document.getElementById("impButton")
+    
+window.onscroll = function() {scrollfunction()}
+
+function scrollfunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop>20){
+        button.style.display = "block";
+    }else{
+        button.style.display = "block";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
---
+++ project/searchBar.js
@@ -0,0 +1,1 @@
+var searchBar = document.getElementById("searchBar")
